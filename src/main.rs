76a68639ff66b87dd00a505d27d22fd9d655685a
littlelinuxoid <footbatra@mail.ru>
2025-09-12 21:20:57 +0300

    Added new macro system to reduce repeating in pattern matching, improved performance.
    File type detection now happens through a procedural macro, reducing the
    overall visual noise of the program and making it infinitely easy to add
    new supported formats. To add new format, it is now not needed to write
    anything but to add new member to central Format enum and give it
    necessary parameters if needed. It is AT MOST three lines of code/file
    format.
    
    To store the amount of lines while processing the files, wclines now
    employs a hash table from file format to overall amount of lines found
    in files of this format. This remarkably simplifies accesing current
    state of work while not harming preformance much.
    
    Also, i started sketching ways to ignore binary files since it is
    useless for you to know how much random sequences of bytes happened to
    decode into a newline character in a .mp3 or .exe file.
    Currently, only ELF (native Linux executable format) files are ignored
    by doing a silly comparison between ELF signature and first bytes of a
    file, but it is not the main purpose of this commit.

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,129 @@
 #![allow(dead_code)]
 use std::{
-    fs::{self, DirEntry, File, FileType},
-    os::linux::fs::MetadataExt,
+    collections::HashMap,
+    ffi::OsStr,
+    fmt::Write,
+    fs::{self, DirEntry, File},
+    io::Read,
     path::Path,
 };
+use wcl_proc_macros::Matcher;
+mod table;
 
-enum SupportedFormat {
-    RustFile,
-    TextFile,
-    CFile,
+const IO_BUFSIZE: usize = 1024 * 256;
+const ELF_SIGNATURE: [u8; 4] = [0x7F, 0x45, 0x4C, 0x46];
+
+#[derive(Debug, Eq, PartialEq, Hash, Matcher)]
+enum Format {
+    #[file_format("rs")]
+    Rust,
+    B,
+    C,
+    D,
+    #[output("Text File")]
+    Txt,
+    Java,
+    #[file_format("rb")]
+    Ruby,
+    Json,
+    #[output("C++")]
+    Cpp,
+    #[file_format("cs")]
+    #[output("C#")]
+    CSharp,
+    #[file_format("hs")]
+    Haskell,
+    #[output("JavaScript")]
+    Js,
+    Ada,
+    Dart,
+    #[file_format("py")]
+    Python,
+    #[output("TypeScript")]
+    Ts,
+    Other,
 }
 
-fn main() -> Result<(), std::io::Error> {
-    let dir_contents = fs::read_dir(".")?.map(|item| item.unwrap());
+#[derive(Debug)]
+struct FileData {
+    extension: Format,
+    lines: usize,
+}
+
+fn count_lines_in_directory<T: AsRef<Path>>(path: T) -> Result<String, std::io::Error> {
+    let dir_contents = fs::read_dir(&path)?.map(|item| item.unwrap());
     let dir_contents = dir_contents.filter(|item| item.metadata().unwrap().is_file());
-    for entry in dir_contents.map(|dir_entry| get_file_extension(dir_entry)) {
-        match entry.as_str() {
-            "txt" => {
-                println!("A txt file!")
+    // use hashmap, because why the fuck not?
+    let mut result: HashMap<Format, usize> = HashMap::new();
+
+    for file_desc in dir_contents {
+        let lines_count = count_lines_in_file(&file_desc)?;
+        result
+            .entry(lines_count.extension)
+            .and_modify(|val| *val += lines_count.lines)
+            .or_insert(lines_count.lines);
+    }
+    Ok(process_hashtable(&result))
+}
+
+fn count_lines_in_file(file: &DirEntry) -> Result<FileData, std::io::Error> {
+    let mut buf: [u8; _] = [0; IO_BUFSIZE + 1];
+    let name = &file.file_name();
+    let mut current_file = File::open(&name).inspect_err(|error| {
+        eprintln!(
+            "could not open the file {}: {error}",
+            &name.to_str().unwrap()
+        );
+    })?;
+
+    // main line counting logic goes here
+    let mut lines_counter = 0;
+    while current_file.read(&mut buf)? > 0 {
+        // try to skip ELF executables as it makes no sense to count lines in binary files
+        // whatsoever.
+        // This approach is dumb and barely extendible
+        // TODO: think of a way to skip most of well-known binary files
+        if buf[0..4] == ELF_SIGNATURE {
+            break;
+        }
+        for character in buf.map(|character: u8| character as char) {
+            match &character {
+                '\n' => lines_counter += 1,
+                _ => continue,
             }
-            "rs" => {
-                println!("A Rust File!")
-            }
-            &_ => println!("Cringe happened!"),
         }
     }
 
+    // println!("{lines_counter} lines in file {}", name.to_str().unwrap());
+
+    Ok(FileData {
+        extension: get_file_ext(file.path().extension()),
+        lines: lines_counter,
+    })
+}
+
+fn get_file_ext(ext: Option<&OsStr>) -> Format {
+    match ext {
+        Some(val) => val
+            .to_owned()
+            .into_string()
+            .unwrap()
+            .parse::<Format>()
+            .unwrap(),
+        // TODO: Binary processing logic goes here
+        None => Format::Other,
+    }
+}
+fn process_hashtable(table: &HashMap<Format, usize>) -> String {
+    let mut answer = String::new();
+
+    for (format, lines) in table {
+        let _ = writeln!(answer, "{} files: {lines}", format.match_to_str());
+    }
+    answer
+}
+fn main() -> Result<(), std::io::Error> {
+    let map = count_lines_in_directory(".")?;
+    println!("{map}");
     Ok(())
-}
-fn get_file_size(f: DirEntry) -> u64 {
-    f.metadata().unwrap().len()
-}
-fn get_file_extension(f: DirEntry) -> String {
-    let p = f.path();
-    let os_ext = p.extension().unwrap();
-    os_ext.to_str().unwrap().to_owned()
 }
---
+++ wcl-proc-macros/src/lib.rs
@@ -0,0 +1,138 @@
+extern crate proc_macro;
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{DataEnum, parse_macro_input};
+
+/// NOTE: This derive macro is not very general since i wrote specifically for wclines, but i can see
+/// its slightly refactored version in a separate library for general_purpose enum handling.
+/// This macro creates two important for wlcines things:
+/// 1. It creates functionality to match enum variants, returning string values.
+///#EXAMPLES
+/// ```
+/// #[derive(Matcher)]
+/// enum Format {
+///     Txt,
+///     Rust,
+///     Js,
+///     Fortran,
+///     Other,
+/// }
+/// fn main() {
+///     let a = Format::Rust;
+///     assert_eq!("Rust", a.match_to_str()),
+/// }
+///```
+/// But it is not just stringify'ing enum variants, although it is by default
+/// For defining match_to_str() function's custom behaviour, this macro provides output
+/// attribute, that changes a string to which the enum variant will be matched.
+///
+/// ```
+/// #[derive(Matcher)]
+/// enum Format {
+///  #[output("Hello World!")]
+///  Rust,
+///  //same enum from previous example
+/// }
+/// fn main() {
+///     let a = Format::Txt,
+///     assert_eq!("Hello World!", a.match_to_str())
+/// }
+/// ```
+/// 2. The second purpose of this macro is essentially a reverse of the first.
+/// It creates an implementation of ```std::str::FromStr``` trait for the enum, making it possible
+/// to call ```parse::<EnumName>()``` on a string literal provides ```file_format```
+/// attribute for defining custom behaviour:
+/// ```
+/// #[derive(Matcher)]
+/// enum Format {
+/// //same enum from previous example
+///     #[file_format("I Love React")]
+///     JavaScript,
+///     C,
+///
+/// }
+/// fn main() {
+///     let a = "c".parse::<Format>().unwrap();
+///     // by default, the lowercase version of enum member's name gets matched to it, making it easy to
+///     // parse most languages' names without defining custom behaviour, since for most languages
+///     // their name is the same as their file extension.
+///     assert_eq!(Format::Rust,a);
+///     let b = "I Love React".parse::<Format>().unwrap();
+///     assert_eq!(Format::JavaScript, b)
+/// }
+///
+/// ```
+///
+///
+#[proc_macro_derive(Matcher, attributes(file_format, output))]
+pub fn matcher_derive(item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as syn::DeriveInput);
+    let name = input.ident;
+    let variants = if let syn::Data::Enum(DataEnum { variants, .. }) = input.data {
+        variants
+    } else {
+        panic!("Enum-only macro!")
+    };
+    //maps enum variants to strings
+    let match_arms_to_str = variants.iter().map(|variant| {
+        let var_name = &variant.ident;
+        let parsed_with_attr = variant.attrs.iter().find_map(|attribute| {
+            if attribute.path().is_ident("output") {
+                attribute
+                    .parse_args::<syn::LitStr>()
+                    .ok()
+                    .map(|lit_str| lit_str.value())
+            } else {
+                None
+            }
+        });
+        let var_str = parsed_with_attr.unwrap_or_else(|| var_name.to_string());
+
+        quote! {
+             #name::#var_name => #var_str,
+        }
+    });
+    //maps strings to enum variants
+    let match_arms_rev = variants.iter().map(|variant| {
+        let var_name = &variant.ident;
+        if var_name == "Other" {
+            quote! { _ => Ok(#name::#var_name), }
+        } else {
+            let parsed_with_attr = variant.attrs.iter().find_map(|attribute| {
+                if attribute.path().is_ident("file_format") {
+                    attribute
+                        .parse_args::<syn::LitStr>()
+                        .ok()
+                        .map(|lit_str| lit_str.value())
+                } else {
+                    None
+                }
+            });
+            let var_str = parsed_with_attr.unwrap_or_else(|| var_name.to_string().to_lowercase());
+
+            quote! {
+                #var_str => Ok(#name::#var_name),
+            }
+        }
+    });
+
+    quote! {
+        impl #name {
+            pub fn match_to_str(&self) -> &'static str {
+                match self {
+                    #(#match_arms_to_str)*
+                }
+            }
+        }
+
+        impl std::str::FromStr for #name {
+            type Err = String;
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                match s{
+                    #(#match_arms_rev)*
+                }
+            }
+        }
+    }
+    .into()
+}
